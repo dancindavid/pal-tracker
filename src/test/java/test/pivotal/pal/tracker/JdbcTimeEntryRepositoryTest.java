8356c31c41964768c7bf6b127adfb9ccf28c99c2
sashinpivotal <sashin@pivotal.io>
2018-11-20 16:07:53 -0500

    Platform Acceleration Lab Add tests for JDBC lab

--- src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
@@ -2,9 +2,7 @@ package test.pivotal.pal.tracker;
 
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import io.pivotal.pal.tracker.JdbcTimeEntryRepository;
-import io.pivotal.pal.tracker.TimeEntry;
-import io.pivotal.pal.tracker.TimeEntryRepository;
+import io.pivotal.pal.tracker.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -26,7 +24,7 @@ public class JdbcTimeEntryRepositoryTest {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
 
-        subject = new JdbcTimeEntryRepository(dataSource);
+        subject = new io.pivotal.pal.tracker.JdbcTimeEntryRepository(dataSource);
 
         jdbcTemplate = new JdbcTemplate(dataSource);
         jdbcTemplate.execute("DELETE FROM time_entries");
